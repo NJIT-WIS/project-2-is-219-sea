bf0c3c32482e83d47d91cb2ecea966be1b5bb53e
Stephen Ebrahim <se6@njit.edu>
2023-04-11 23:17:00 -0400

    created nextjs config and completed page wrapper

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,6 +5,12 @@ const siteMetadata = {
   description: "temp description...",
   language: "en-us",
   theme: "system",
+  email: "address@yoursite.com",
+  github: "https://github.com",
+  twitter: "https://twitter.com/Twitter",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com",
 };
 
 module.exports = siteMetadata;
