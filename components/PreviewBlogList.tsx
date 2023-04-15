afd4e380fc4bfa3c97596711f8265a2c527a2290
Ebram Mekhail <ebrammekhail@gmail.com>
2023-04-15 16:13:15 -0400

    Implmented Sanity with current blog created several test posts

---
+++ components/PreviewBlogList.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import {usePreview} from 'lib/sanity.preview'
+import {query, BlogList} from 'components/BlogList'
+
+export default function PreviewBlogList({token}) {
+  const data = usePreview(token, query)
+  return <BlogList data={data} />
+}
--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,8 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-  basePath: "/project-2-is-219-sea/studio",
+  // basePath: "/project-2-is-219-sea/studio", // This is production path change to studio in dev.
+  basePath: "/studio",
   name: 'Neural_Nexus_Studio',
   title: 'Neural Nexus Studio',
 
