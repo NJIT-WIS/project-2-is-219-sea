44a0315aa175c950d1f6ee4b801f00e95a9e65fa
Aryan Nesti <98928740+AryanNesti@users.noreply.github.com>
2023-04-19 17:41:12 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ module.exports = {
 
     return config;
   },
+  env: {
+    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+    MAILCHIMP_API_SERVER: process.env.MAILCHIMP_API_SERVER,
+    MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
+  },
 };
